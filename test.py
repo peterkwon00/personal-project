e320704b53e53133c80e638c4ecfe4e245d19e99
peterkwon00 <61730841+peterkwon00@users.noreply.github.com>
2020-04-24 21:33:19 +0900

    전체 업데이트

--- test.py
+++ test.py
@@ -1,1 +1,14 @@
-print ('hello world')
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+chrome_options = Options()
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--disable-dev-shm-usage')
+
+driver = webdriver.Chrome('./chromedriver', chrome_options=chrome_options)
+
+driver.get('http://www.naver.com')
+driver.implicitly_wait(3)
+driver.get_screenshot_as_file('naver.png')
+driver.quit()
